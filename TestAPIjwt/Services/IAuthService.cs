7f1af5431da5e076b36162a1c92c14f18c3b471a
AhmedRabieG <ahmedrabie010@gmail.com>
2021-03-21 17:26:54 +0200

    Add project files.

---
+++ TestAPIjwt/Services/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TestAPIjwt.Models;
+
+namespace TestAPIjwt.Services
+{
+     public interface IAuthService
+    {
+        Task<AuthModel> RegisterAsync(RegisterModel model);
+        Task<AuthModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
+    }
+}
